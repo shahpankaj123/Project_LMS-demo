805856b766b79b2709805cbee77ff7202fe12ee4
Aaryan Shah <aaryanshah615@gmail.com>
2024-04-08 21:14:50 +0545

    first commit

--- lms/model1.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import timedelta
-
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.fields import GenericRelation
-
-
